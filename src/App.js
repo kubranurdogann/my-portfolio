0ca137d5ee37bd14944dff0d73b198663b10cf97
Kübranur <kubra.nur46300@gmail.com>
2025-10-21 13:37:38 +0300

    Updated

--- src/App.js
+++ src/App.js
@@ -61,12 +61,9 @@ const downloadCV = () => {
         <div className="col-md-9 col-sm-12">
           <h1 className="pb-3">Hi, I'm Kubranur Dogan!</h1>
           <p className="text-justify">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged.{" "}
+            Hi! I’m a computer engineering graduate passionate about web development and new technologies.
+            I love learning, solving problems, and working as part of a creative team.
+            <br></br> I’m always ready to take on new challenges and grow in the tech world.{" "}
           </p>
           <div className=" d-flex justify-content-start align-items-center gap-3">
             <button className="btn btn-type" onClick={downloadCV}>My CV</button>
